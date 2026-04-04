804578d82322f4d4765c82b0b6d2cce6f270ad18
enzosdev <enzohfs@gmail.com>
2026-04-04 19:02:09 -0300

    feat: add working navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,11 @@ function Header() {
       </section>
 
       <nav aria-label="Principal">
-        <a href="#">Inicio</a>
+        <a href="/">Home</a>
         <a href="#">Catalogo</a>
         <a href="#">Planos</a>
         <a href="#">Contato</a>
+        <a href="/login">Login</a>
       </nav>
     </header>
   );
