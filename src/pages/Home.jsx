2bf63d1f945655ce0ddd9db6af296717a2a63ea7
enzosdev <101185305+Enzosantos04@users.noreply.github.com>
2026-05-02 17:50:14 -0300

    feat: connect application to api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,59 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../components/modules/Home.module.css";
 import CardQuadrinho from "../components/CardQuadrinho";
 import Header from "../components/Header";
 
-const listaQuadrinhos = [
-  {
-    id: 1,
-    titulo: "Homem-Aranha",
-    imagem:
-      "https://tse2.mm.bing.net/th/id/OIP.oVaptcS3N5aw7cuDsHtrHQHaLa?rs=1&pid=ImgDetMain&o=7&rm=3",
-    categoria: "Ação",
-    preco: 15,
-  },
-  {
-    id: 2,
-    titulo: "Batman",
-    imagem:
-      "https://i.pinimg.com/736x/ac/64/4b/ac644b13096352f91f92c9dfb1cc0093.jpg",
-    categoria: "Aventura",
-    preco: 20,
-  },
-  {
-    id: 3,
-    titulo: "Superman",
-    imagem:
-      "https://comicvine.gamespot.com/a/uploads/scale_medium/11120/111202620/4338422-9943360583-60175.jpg",
-    categoria: "Ação",
-    preco: 18,
-  },
-  {
-    id: 4,
-    titulo: "Mulher-Maravilha",
-    imagem:
-      "https://zonafantasmanet.wordpress.com/wp-content/uploads/2023/08/wonder-woman-2011-2016-a-twist-of-fate-v8-000.jpg",
-    categoria: "Aventura",
-    preco: 22,
-  },
-];
+const mykey = import.meta.env.VITE_COMIC_VINE_API_KEY;
 
 export default function Home() {
+  const [quadrinho, setQuadrinho] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      // Usando o endpoint /volumes/ para buscar quadrinhos diversos (HQs variadas)
+      const apiUrl = `https://comicvine.gamespot.com/api/volumes/?api_key=${mykey}&format=json&limit=20&sort=date_added:desc&field_list=id,name,image,publisher,description`;
+
+      // cors proxy eh um mediador para evitar problemas de CORS, pois a API do Comic Vine não suporta requisições diretas do frontend
+      //entao eh feita uma requisição para o cors proxy, que entao faz a request para a api
+      //encodeURIComponent é usado para garantir que a URL seja corretamente formatada ao ser passada como parâmetro para o proxy
+      const response = await fetch(
+        `https://corsproxy.io/?${encodeURIComponent(apiUrl)}`,
+      );
+
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+
+      const data = await response.json();
+
+      const quadrinhosFormatados = data.results.map((item) => ({
+        id: item.id,
+        titulo: item.name,
+        imagem: item.image?.medium_url || item.image?.original_url,
+        categoria: item.publisher?.name || "Quadrinhos",
+        description: item.description,
+        preco: "29,90",
+      }));
+
+      setQuadrinho(quadrinhosFormatados);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Header />
       <section className={styles.hero}>
         <h2>Explore os melhores quadrinhos do momento</h2>
-
         <div className={styles.carrossel}>
-          {listaQuadrinhos.map((quadrinho) => (
+          {quadrinho.map((item) => (
             <CardQuadrinho
-              key={quadrinho.id}
-              id={quadrinho.id}
-              titulo={quadrinho.titulo}
-              imagem={quadrinho.imagem}
-              categoria={quadrinho.categoria}
-              preco={quadrinho.preco}
+              key={item.id}
+              id={item.id}
+              titulo={item.titulo}
+              imagem={item.imagem}
+              categoria={item.categoria}
+              preco={item.preco}
             />
           ))}
         </div>
